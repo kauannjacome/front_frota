bf2bca666c88269864e4fbb045d597c244771328
kauannjacome <kauannjacome@hotmail.com>
2025-05-19 23:50:55 -0300

    primeiro heroku

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL || 'https://frotasimples-bbc0f36789e8.herokuapp.com/',
 });
 
 export default api;
