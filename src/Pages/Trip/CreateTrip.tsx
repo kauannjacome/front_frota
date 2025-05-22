db474ee5c4a8c6ed4a2196b1061f2a526a25c669
kauannjacome <kauannjacome@hotmail.com>
2025-05-21 22:06:52 -0300

    criação de horario 2

--- src/Pages/Trip/CreateTrip.tsx
+++ src/Pages/Trip/CreateTrip.tsx
@@ -16,8 +16,8 @@ export default function CreateTrip() {
     try {
       const payload = {
         ...values,
-        journey_start: values.journey_start.format('YYYY-MM-DDTHH:mm:ssZ'),
-        journey_back: values.journey_back.format('YYYY-MM-DDTHH:mm:ssZ'),
+        journey_start: values.journey_start.toISOString(true),
+        journey_back: values.journey_back.toISOString(true),
         persons: persons.map((p) => ({
           person_id: p.id,
           dropoff_location: p.dropoff_location,
